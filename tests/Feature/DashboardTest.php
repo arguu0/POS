5d3e200c4d53ca6840ae12d9b15525541b901228
Arguu <htetm2714@gmail.com>
2026-08-16 21:47:31 +0630

    User and Store relation

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Store;
 use App\Models\User;
 
 test('guests are redirected to the login page', function () {
@@ -9,8 +10,23 @@ test('guests are redirected to the login page', function () {
 
 test('authenticated users can visit the dashboard', function () {
     $user = User::factory()->create();
+
+    $store = Store::factory()->create([
+        'user_id' => $user->id,
+    ]);
+
+    $user->refresh();
+
+    dump([
+        'user_id' => $user->id,
+        'store_id' => $store->id,
+        'store_user_id' => $store->user_id,
+        'relationship_store_id' => $user->store?->id,
+    ]);
+
     $this->actingAs($user);
 
     $response = $this->get(route('dashboard'));
+
     $response->assertOk();
 });
