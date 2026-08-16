b3c6f0fce3efad768439ce55a08e609ffba0793f
Arguu <htetm2714@gmail.com>
2026-08-16 22:07:01 +0630

    testing

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
+use Database\Factories\StoreFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+
+
 class Store extends Model
 {
+    /**
+     * @use HasFactory<StoreFactory>
+     */
     use HasFactory;
     protected $fillable = [
         'name'
