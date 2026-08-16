fa3d6f6c41d61d3fac625512c305abb0fe22d091
Arguu <htetm2714@gmail.com>
2026-08-16 22:11:05 +0630

    testing_2

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -15,15 +15,6 @@ test('authenticated users can visit the dashboard', function () {
         'user_id' => $user->id,
     ]);
 
-    $user->refresh();
-
-    dump([
-        'user_id' => $user->id,
-        'store_id' => $store->id,
-        'store_user_id' => $store->user_id,
-        'relationship_store_id' => $user->store?->id,
-    ]);
-
     $this->actingAs($user);
 
     $response = $this->get(route('dashboard'));
